0a037c8586ecb83016f339cdbf2fbca1301cad60
Olga <olga.slepova87@gmail.com>
2025-10-27 20:42:18 +0100

    feat(strings): implement reverseString using .split().reverse().join()

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -260,8 +260,8 @@ function formatTime(minutes, seconds) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  return String(str).split('').reverse().join('');
 }
 
 /**
