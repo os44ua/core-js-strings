8e06bb77e3325efd320f0bde2d433073d096732c
Olga <olga.slepova87@gmail.com>
2025-10-27 21:50:31 +0100

    feat(strings): implement unbracketTag using .slice()

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -438,8 +438,8 @@ function extractNameFromTemplate(value) {
  *   unbracketTag('<span>') => 'span'
  *   unbracketTag('<a>') => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return String(str).slice(1, -1);
 }
 
 /**
