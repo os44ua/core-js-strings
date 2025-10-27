445a2fc31eb3abc03d7a095712051732b122444b
Olga <olga.slepova87@gmail.com>
2025-10-27 20:20:37 +0100

    feat(strings): implement repeatString using .repeat

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -133,8 +133,11 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (typeof times !== 'number' || times <= 0) {
+    return '';
+  }
+  return String(str).repeat(times);
 }
 
 /**
